c3384b69816b56c0e2ea48c08cd505ee1d28148f
Vladimir-Fedorov <neroazzurri@mail.ru>
2025-01-20 08:18:51 +0400

    added mocks for the main window menu

--- internal/fyne/app/app.go
+++ internal/fyne/app/app.go
@@ -1,22 +1,63 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+const (
+	appIcon = "/home/vovanshaitan/Learning/GoProjects/github.com/VovanShaitan/totolab/assets/icons/flask.png"
+)
+
 func main() {
 	a := app.New()
+
 	w := a.NewWindow("Hello")
+	w.Resize(fyne.NewSize(400, 400))
+
+	setAppIcon(w)
+
+	fileOpen, fileSave := createFileMenuButtons()
+
+	menuFile := fyne.NewMenu("File", fileOpen, fileSave)
+
+	helpAbout := fyne.NewMenuItem("About program", func() {
+		fmt.Println("Program info")
+	})
+
+	menuHelp := fyne.NewMenu("Help", helpAbout)
+
+	menuMain := fyne.NewMainMenu(menuFile, menuHelp)
+
+	w.SetMainMenu(menuMain)
 
 	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
+	content := container.NewVBox(
 		hello,
 		widget.NewButton("Hi!", func() {
 			hello.SetText("Welcome :)")
 		}),
-	))
+	)
+	w.SetContent(content)
 
 	w.ShowAndRun()
+}
+
+func createFileMenuButtons() (*fyne.MenuItem, *fyne.MenuItem) {
+	fileOpen := fyne.NewMenuItem("Open File", func() {
+		fmt.Println("File opened")
+	})
+	fileSave := fyne.NewMenuItem("Save File", func() {
+		fmt.Println("File saved")
+	})
+	return fileOpen, fileSave
+}
+
+func setAppIcon(w fyne.Window) {
+	ic, _ := fyne.LoadResourceFromPath(appIcon)
+	w.SetIcon(ic)
 }
